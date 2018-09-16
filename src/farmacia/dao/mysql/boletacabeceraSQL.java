811bc6340ba3a28c2712e89c6bcc1061e7506be4
jaico-git <juniorefrain@hotmail.com>
2018-09-16 16:41:44 -0500

    SQL boleta cabecera

--- src/farmacia/dao/mysql/boletacabeceraSQL.java
+++ src/farmacia/dao/mysql/boletacabeceraSQL.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class boletacabeceraSQL implements boletacabeceraDAO{
 
-    
-
+    final String INSERT = "INSERT INTO boletacabecera(correlativoboleta, numeroboleta, fechaemisionboleta, idpersonacliente, idempleado, status) "
+    +"VALUES (?, ?, ?, ?, ?, ?) ";
+    final String UPDATE = "UPDATE boletacabecera SET correlativoboleta = ?, numeroboleta = ?, fechaemisionboleta = ?, idpersonacliente = ?, idempleado = ?, status = ?";
+    final String DELETE = "UPDATE boletacabecera SET status = 0 WHERE idboletacabecera = ?";
+    final String GETALL = "SELECT * FROM boletacabecera WHERE status = 1";//solo obtiene los activos 
+    final String GETONE = "SELECT * FROM empresa WHERE idempresa = ?";
 
     @Override
     public void insertar(boletacabecera obj) {
