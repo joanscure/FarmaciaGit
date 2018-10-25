9144c58cd957f6774ca15bfd5227a336030cb64a
jaico-git <juniorefrain@hotmail.com>
2018-10-25 16:51:57 -0500

    Paquete hibernate. Modelo

---
+++ src/farmacia/hibernate/util/PruebaRegistros.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia.hibernate.util;
+
+import farmacia.hibernate.modelo.Persona;
+import farmacia.util.NewHibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author jaico
+ */
+public class PruebaRegistros {
+    
+        public static void main(String[] args) {
+//        SessionFactory sessionFac = NewHibernateUtil.getSessionFactory();
+//        Session sesion1;
+//        sesion1 = sessionFac.openSession();
+//        Transaction tx = sesion1.beginTransaction();
+//        sesion1.save(crearPersona());
+//        tx.commit();
+//        sesion1.close();
+//        sessionFac.close();
+        System.out.println("1111");
+//            System.out.println(sesion1.isOpen());
+//            System.out.println(sessionFac.isClosed());
+//         
+    }
+    
+    private static Persona crearPersona(){
+        Persona p;
+        p = new Persona("jose", "perez", "gonzales", "12345678", true);
+        return p;
+    }
+}
