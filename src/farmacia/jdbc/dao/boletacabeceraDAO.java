79bcac104e75f60a488f0a9ef369b7487e31310a
joanscure <joan.leyton08@gmail.com>
2018-10-25 22:01:06 -0500

    frm consultas corregida

--- src/farmacia/jdbc/dao/boletacabeceraDAO.java
+++ src/farmacia/jdbc/dao/boletacabeceraDAO.java
@@ -1,9 +1,9 @@
 package farmacia.jdbc.dao;
 
 import farmacia.jdbc.modelado.boletacabecera;
-import java.util.Date;
 import java.util.List;
 
 public interface boletacabeceraDAO extends DAO<boletacabecera, Long> {
 
+    public List<boletacabecera> obtenerportiempo(java.util.Date min,java.util.Date max) throws DAOException ;
 }
